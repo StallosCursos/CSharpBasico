8c19c5c9a25696153fc4c03eb18df7ccb9d2c291
stallosdev <stallosdev@outlook.com>
2021-02-18 07:22:21 -0300

    Adicionado console terminal basico

---
+++ Exemplo 1 - Entrada e Saida Terminal/Terminal/Terminal/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Terminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exemplo de escrita no terminal
+            Console.WriteLine("Digite seu nome!");
+
+            // Recebe a linha digitada no terminal
+            string Nome = Console.ReadLine();
+
+            // Imprime a variavel recebida
+            Console.WriteLine("Olá: " + Nome);
+        }
+    }
+}
