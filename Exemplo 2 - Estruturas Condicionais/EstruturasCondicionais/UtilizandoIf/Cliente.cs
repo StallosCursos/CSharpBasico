ff79d1708f587a22e85f3205566e71e1a25940d2
stallosdev <stallosdev@outlook.com>
2021-02-23 18:47:30 -0300

    Adicionado Exemplo com IF

---
+++ Exemplo 2 - Estruturas Condicionais/EstruturasCondicionais/UtilizandoIf/Cliente.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Cliente
+{
+    private DateTime _datanascimento;
+    private bool _maiorIdade;
+
+    public string Nome { get; set; }
+    public string Cpf { get; set; }
+    public bool MaiorIdade { get; set; }
+
+    public DateTime DataNacimento 
+    {
+        get => this._datanascimento;
+        set 
+        { 
+           this._datanascimento = value;
+           this._maiorIdade = DefinirMaioridade(value);
+        } 
+    }
+
+    public Pagamento Pagamento { get; set; }
+
+    private bool DefinirMaioridade(DateTime dataNascimento)
+    {
+        int idade = DateTime.Now.Year - dataNascimento.Year;
+
+        // DayOfYear - Retorna um inteiro representando o dia do ano, exemplo 01/01/xxxx = 1
+        if (DateTime.Now.DayOfYear < dataNascimento.DayOfYear)
+        {
+            idade = idade - 1;
+        }
+
+        return idade > 18;
+    }
+}
---
+++ Exemplo 2 - Estruturas Condicionais/EstruturasCondicionais/UtilizandoIf/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace UtilizandoIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente cliente = new Cliente();
+
+            Console.WriteLine("Olá informe seu nome. ");
+            cliente.Nome = Console.ReadLine();
+
+            Console.WriteLine($"Muito bem {cliente.Nome} Informe agora sua data de nascimento");
+            cliente.DataNacimento = Convert.ToDateTime(Console.ReadLine());
+
+            Console.WriteLine($"Muito bem {cliente.Nome} Informe seu cpf");
+            cliente.Cpf = Console.ReadLine();
+
+            Console.WriteLine($"Muito bem {cliente.Nome} deseja realizar um pagamento ? S - sim ou N - Não ");
+            string realizarPagamento = Console.ReadLine();
+
+            if (realizarPagamento.ToUpper() == "S")
+            {
+                if (cliente.MaiorIdade)
+                {
+                    cliente.Pagamento = new Pagamento();
+
+                    Console.WriteLine($"Muito bem {cliente.Nome} informe a forma de pagamento");
+                    Console.WriteLine("1 - Debito");
+                    Console.WriteLine("2 - Credito");
+                    Console.WriteLine("3 - Pix");
+                    Console.WriteLine("4 - Dinheiro");
+                    Console.WriteLine("5 - Transferencia");
+
+                    int tipoPagamento = Convert.ToInt32(Console.ReadLine());
+
+                    if (tipoPagamento == 1)
+                        cliente.Pagamento.TipoPagamento = TipoPagamento.Debito;
+                    else if (tipoPagamento == 2)
+                        cliente.Pagamento.TipoPagamento = TipoPagamento.Credito;
+                    else if (tipoPagamento == 3)
+                        cliente.Pagamento.TipoPagamento = TipoPagamento.Pix;
+                    else if (tipoPagamento == 4)
+                        cliente.Pagamento.TipoPagamento = TipoPagamento.Dinheiro;
+                    else if (tipoPagamento == 5)
+                        cliente.Pagamento.TipoPagamento = TipoPagamento.Transferencia;
+                    else
+                        Console.WriteLine("Tipo de pagamento não registrado");
+
+                    cliente.Pagamento.DataPagamento = DateTime.Now;
+
+                    Console.WriteLine("Por favor digite o valor do pagamento ");
+                    cliente.Pagamento.Valor = Convert.ToDecimal(Console.ReadLine());
+
+                    if (cliente.Pagamento.DataPagamento <= cliente.Pagamento.DataVencimento)
+                    {
+                        RealizarPagamento(cliente.Pagamento);
+                    }
+                    else
+                        Console.WriteLine("Pagamento não pode ser realizado por estar vencido");
+                }
+                else
+                    Console.WriteLine($"Muito bem {cliente.Nome} | Pagamento só pode ser realizado por clientes com mais de 18 anos");
+            }
+            else
+                Console.WriteLine($"Muito bem {cliente.Nome} | CPF: {cliente.Cpf} | estamos fechando o programa");
+
+            Console.ReadKey();
+        }
+
+        private static void RealizarPagamento(Pagamento pagamento)
+        {
+            Console.WriteLine($"Pagamento de {pagamento.Valor} realizado no dia {pagamento.DataPagamento} vencimento {pagamento.DataVencimento}");
+        }
+    }
+}
